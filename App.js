7cde97bb2b7603084354b9442eae3ef887cfe2a5
jaesebaese <jasmin.hochuli@uzh.ch>
2022-03-31 16:19:48 +0200

    redux installed

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux-thunk';
+import thunk from 'redux-thunk';
+import rootReducer from './src/redux/reducers';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to roomeight!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Welcome to roomeight!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </Provider>
   );
 }
 
