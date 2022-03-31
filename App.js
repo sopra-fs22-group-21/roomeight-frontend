a7397ae227cfd4b7a97581d8258c849b98fcea05
jaesebaese <jasmin.hochuli@uzh.ch>
2022-03-31 17:07:39 +0200

    AuthenticationMenu added

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './src/redux/reducers';
 
---
+++ src/components/views/AuthenticationMenu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text, Button} from 'react-native'
+
+export default function AuthenticationMenu(){
+    return(
+        <View >
+            <Button>Log in</Button>
+        </View>
+    )
+}
