1d385f80af14b9b56d58440ebe56fab7dae29a5d
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2022-04-04 18:00:38 +0200

    reducer fix

--- src/screens/signup/index.js
+++ src/screens/signup/index.js
@@ -137,6 +137,7 @@ const Signup = ({ navigation }) => {
                         onPress={() => {
                             dispatch(postUserprofile(user));
                             console.log('posting');
+                            console.log(user);
                         }}
                     >
                         Sign Up
@@ -145,9 +146,9 @@ const Signup = ({ navigation }) => {
                         title="Already have an account"
                         onPress={() => navigation.navigate('Login')}
                     />
-                    <Text>{loading}</Text>
-                    <Text>{userProfile}</Text>
-                    <Text>{error}</Text>
+                    <Text>isLoading: {loading.toString()}</Text>
+                    <Text>Userprofile: {JSON.stringify(userProfile)}</Text>
+                    <Text>Error: {JSON.stringify(error)}</Text>
                 </ScrollView>
             </KeyboardAvoidingView>
         </View>
