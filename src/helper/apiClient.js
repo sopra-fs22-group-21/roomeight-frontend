410ac53bff50e1455d69e1e6936cf6c77fa8e90e
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2022-04-07 16:43:04 +0200

    added jest config

--- src/helper/apiClient.js
+++ src/helper/apiClient.js
@@ -7,10 +7,9 @@ const API_URL = (env = Constants.manifest.releaseChannel) => {
     } else {
         return 'https://us-central1-roomeight-9cd94.cloudfunctions.net';
     }
-}
+};
 
 const apiClient = () => {
-    
     /* const API_URL = 'https://us-central1-roomeight-9cd94.cloudfunctions.net';
     const DEV_URL =
         'https://b8585bf5-1579-433d-aec8-d6ad6bdfb349.mock.pstmn.io'; */
