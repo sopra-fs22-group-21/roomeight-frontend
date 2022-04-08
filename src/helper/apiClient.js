a2a70f7d8a6480a9b981c292e7b3b9848231d834
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2022-04-08 16:22:50 +0200

    added auth header via interceptor

--- src/helper/apiClient.js
+++ src/helper/apiClient.js
@@ -1,23 +1,49 @@
 import axios from 'axios';
 import Constants from 'expo-constants';
+import { CombinedState } from 'redux';
+import { auth } from '../../firebase/firebase-config';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useSelector } from 'react-redux';
 
 const API_URL = (env = Constants.manifest.releaseChannel) => {
-    if (env === __DEV__) {
+    /* if (env === __DEV__) {
+        console.log('dev');
         return 'https://b8585bf5-1579-433d-aec8-d6ad6bdfb349.mock.pstmn.io';
     } else {
+        console.log('prod');
         return 'https://us-central1-roomeight-9cd94.cloudfunctions.net';
+    } */
+
+    return 'https://b8585bf5-1579-433d-aec8-d6ad6bdfb349.mock.pstmn.io';
+};
+
+async function userToken(){
+    const user = auth.currentUser;
+    if (user) {
+        console.log("logged in");
+        const token = await user.getIdToken()
+        return token;
+    }else{
+        console.log("no user")
+        return null;
     }
 };
 
 const apiClient = () => {
-    /* const API_URL = 'https://us-central1-roomeight-9cd94.cloudfunctions.net';
-    const DEV_URL =
-        'https://b8585bf5-1579-433d-aec8-d6ad6bdfb349.mock.pstmn.io'; */
 
     const axiosInstance = axios.create({
-        baseURL: API_URL,
+        baseURL: API_URL(),
         responseType: 'json',
     });
+
+    axiosInstance.interceptors.request.use(async (req) => {
+        const accessToken = await userToken();
+        if (accessToken) {
+            req.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return req;
+    });
+
     return axiosInstance;
 };
 
