217f3b476f7885855cde41ea706f7379078a86dc
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2022-04-18 17:06:20 +0200

    finished addPicture screen

--- src/screens/addPictures/index.js
+++ src/screens/addPictures/index.js
@@ -1,33 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { View, Button, Image } from 'react-native';
+import { View, Button, Image, FlatList } from 'react-native';
 import en from '../../resources/strings/en.json';
 import { TextBlock, Heading, Title, Container } from '../../components/theme';
 import * as ImagePicker from 'expo-image-picker';
 import Constants from 'expo-constants';
-import ImageInput from '../../components/imageInput';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uploadImages } from '../../redux/actions/uploadImage';
 import { NavigationButtons } from '../../components/navigationButtons';
 import { SecondaryButton } from '../../components/button';
 import PictureInput from '../../components/profilePictureInput';
+import { PickImage } from '../../helper/imageHandler';
 
 const AddPictures = ({ navigation }) => {
-    const [images, setImages] = useState([]);
+    const userImages = useSelector(
+        (state) => state.userprofileState.userProfile.pictureReferences
+    );
+    const [images, setImages] = useState(
+        userImages ? userImages : ['', '', '', '']
+    );
 
     const dispatch = useDispatch();
-    const PickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-        });
-        if (!result.cancelled) {
-            console.log(images);
-            console.log(result);
-            setImages([...images, result.uri]);
-        }
-    };
+
+    function deletePicture(index) {
+        let updated = [...images];
+        updated[index] = '';
+        setImages(updated);
+    }
+
+    async function addPicture(index) {
+        const uri = await PickImage();
+        let updated = [...images];
+        updated[index] = uri;
+        setImages(updated);
+    }
 
     useEffect(async () => {
         if (Platform.OS !== 'web') {
@@ -41,35 +46,51 @@ const AddPictures = ({ navigation }) => {
         }
     }, []);
 
-    let first;
-    console.log(images);
-    if (images[0]) {
-        first = (
-            <Image
-                source={{ uri: images[0] }}
-                style={{ width: 80, height: 100 }}
-            />
-        );
-    }
-    let second;
-    if (images[1]) {
-        second = (
-            <Image
-                source={{ uri: images[1] }}
-                style={{ width: 80, height: 100 }}
-            />
-        );
-    }
+    const pictureSelectors = [
+        {
+            index: 0,
+            image: images[0],
+        },
+        {
+            index: 1,
+            image: images[1],
+        },
+        {
+            index: 2,
+            image: images[2],
+        },
+        {
+            index: 3,
+            image: images[3],
+        },
+    ];
+
     return (
         <Container>
             <Title>{en.addPictures.heading}</Title>
-            <PictureInput />
-            {first || <ImageInput onPress={() => PickImage()} />}
-            {second || <ImageInput onPress={() => PickImage()} />}
+
+            <FlatList
+                data={pictureSelectors}
+                keyExtractor={(item) => item.index}
+                numColumns={2}
+                columnWrapperStyle={{
+                    justifyContent: 'space-evenly',
+                    paddingBottom: 30,
+                }}
+                renderItem={({ item }) => (
+                    <PictureInput
+                        variant="additional"
+                        onPressDelete={() => deletePicture(item.index)}
+                        onPressSelect={() => addPicture(item.index)}
+                        image={item.image}
+                    />
+                )}
+            />
+
             <NavigationButtons
                 onPressBack={() => navigation.goBack()}
                 onPressNext={() => {
-                    //dispatch(uploadImages(images, 'userprofile'));
+                    dispatch(uploadImages(images, 'userprofile'));
                     navigation.navigate('Discover');
                 }}
             />
