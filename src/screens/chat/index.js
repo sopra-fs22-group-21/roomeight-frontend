7fbb209fbf5476481345f5f74f2713a46829ae4c
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2022-04-20 19:32:58 +0200

    adds components

--- src/screens/chat/index.js
+++ src/screens/chat/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
 import { Container, Screen } from '../../components/theme';
 
 const Chat = ({ navigation }) => {
+
+    const chats = useSelector(state => state.chatState.chats);
+
+    const renderItem = ({ item }) => {
+        return (
+            <ChatListItem chat={item}/>
+        );
+    };
+
     return (
         <Screen navigation={navigation} showFooter>
             <Container>
                 <Text>Chat</Text>
+                <FlatList
+                    data={chats}
+                    removeClippedSubviews
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
+                />
             </Container>
         </Screen>
     );
