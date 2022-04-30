260221638050a8a0d04cad084392f82d3f4994fd
jaesebaese <jasmin.hochuli@uzh.ch>
2022-04-30 17:41:14 +0200

    add roomie redux

--- src/redux/actions/postFlatprofile.js
+++ src/redux/actions/postFlatprofile.js
@@ -1,6 +1,7 @@
 import { updateProfile } from './updateActions';
 import apiClient from '../../helper/apiClient';
 import * as Constants from '../constants';
+import { getFlatprofile } from './getFlatprofiles';
 
 const postFlatprofileRequest = () => ({
     type: Constants.POST_FLATPROFILE_REQUEST,
@@ -13,6 +14,20 @@ const postFlatprofileSuccess = (response) => ({
 
 const postFlatprofileFailure = (error) => ({
     type: Constants.POST_FLATPROFILE_FAILURE,
+    payload: error,
+});
+
+const postRoommateToFlatRequest = () => ({
+    type: Constants.POST_ROOMMATE_TO_FLAT_REQUEST,
+});
+
+const postRoommateToFlatSuccess = (response) => ({
+    type: Constants.POST_ROOMMATE_TO_FLAT_SUCCESS,
+    payload: response,
+});
+
+const postRoommateToFlatFailure = (error) => ({
+    type: Constants.POST_ROOMMATE_TO_FLAT_FAILURE,
     payload: error,
 });
 
@@ -32,6 +47,9 @@ export const postFlatprofile = (requestBody) => (dispatch) => {
     let references = requestBody.pictureReferences;
     delete requestBody.pictureReferences;
 
+    let emails = requestBody.roommateEmails;
+    delete requestBody.roommateEmails;
+
     console.log('requestBody:');
     console.log(requestBody);
     apiClient()
@@ -45,9 +63,29 @@ export const postFlatprofile = (requestBody) => (dispatch) => {
             dispatch(
                 updateProfile(update, 'flatprofile', response.data.profileId)
             );
+            emails.forEach((email) => {
+                dispatch(postRoommateToFlat(email));
+            });
         })
         .catch((error) => {
             console.log('error post flatprofile');
             dispatch(postFlatprofileFailure(error));
+        });
+};
+
+export const postRoommateToFlat = (email) => (dispatch) => {
+    dispatch(postRoommateToFlatRequest());
+    apiClient()
+        .post(`/flatprofiles/roommate/${email}`, {})
+        .then((response) => {
+            console.log('could add roommate with email ' + email);
+            console.log(JSON.stringify(response.data));
+
+            dispatch(postRoommateToFlatSuccess(response.data));
+            dispatch(getFlatprofile());
+        })
+        .catch((error) => {
+            console.log('error post flatprofile');
+            dispatch(postRoommateToFlatFailure(error));
         });
 };
