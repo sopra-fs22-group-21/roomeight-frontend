d290f2c33f060e6ba0a9a818ada93a3d1fe7238e
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2022-05-20 18:13:26 +0200

    removes unused method

--- src/redux/actions/notificationActions.js
+++ src/redux/actions/notificationActions.js
@@ -5,57 +5,6 @@ import { database } from '../../../firebase/firebase-config';
 import * as Constants from '../constants';
 import { getCurrentUserprofile } from './getUserprofiles';
 
-/**
- * Listens to the changes in the database for new likes and new matches
- * @dispatches {@link Constants.MATCHES_AND_LIKES_LISTENER_SUCCESS} on listener start
- * @dispatches {@link Constants.NEW_MATCH} if a new match is generated
- * @dispatches {@link Constants.NEW_LIKE} if a new like is generated
- */
-export const matchesAndLikesListener = () => (dispatch, getState) => {
-    dispatch({
-        type: Constants.MATCHES_AND_LIKES_LISTENER_STARTED,
-    });
-    const isSearching = getState().userprofileState.userprofile.isSearchingRoom;
-    if (isSearching) {
-        const uid = getState().authState.auth.uid;
-        const changesReference = ref(database, `/changes/${uid}`);
-        onChildChanged(changesReference, (snapshot) => {
-            if (!snapshot.exists()) return;
-            if (snapshot.key === 'newestMatch') {
-                dispatch({
-                    type: Constants.NEW_MATCH,
-                    payload: snapshot.val(),
-                });
-                dispatch(getCurrentUserprofile());
-            } else if (snapshot.key === 'newestLike') {
-                dispatch({
-                    type: Constants.NEW_LIKE,
-                    payload: snapshot.val(),
-                });
-                dispatch(getCurrentUserprofile());
-            }
-        });
-    } else {
-        const flatId = getState().userprofileState.userprofile.flatId;
-        const changesReference = ref(database, `/changes/${flatId}`);
-        onChildChanged(changesReference, (snapshot) => {
-            if (snapshot.key() === 'newestMatch') {
-                dispatch({
-                    type: Constants.NEW_MATCH,
-                    payload: snapshot.val(),
-                });
-                dispatch(getCurrentUserprofile());
-            } else if (snapshot.key() === 'newestLike') {
-                dispatch({
-                    type: Constants.NEW_LIKE,
-                    payload: snapshot.val(),
-                });
-                dispatch(getCurrentUserprofile());
-            }
-        });
-    }
-};
-
 export const notificationsListener = () => (dispatch, getState) => {
     dispatch({
         type: Constants.NOTIFICATIONS_LISTENER_STARTED,
